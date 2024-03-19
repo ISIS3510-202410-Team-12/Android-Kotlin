5bbe9804193e8e812c501a605248ed76dd32023f
Lucciano Franco <l.francom@uniandes.edu.co>
2024-03-19 12:30:59 -0500

    Prueba Final desde Android

--- app/src/main/java/com/kotlin/unischedule/MainActivity.kt
+++ app/src/main/java/com/kotlin/unischedule/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello bb te amo $name!",
+        text = "Hello bb te amo chao tu $name!",
         modifier = modifier
     )
 }
