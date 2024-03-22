01f485216856d853cc4481f6d86f30ff3317b040
Laudarias <l.ariasf@uniandes.edu.co>
2024-03-22 03:01:57 -0500

    Initial commit, Auth fisrt set up

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.kotlin
 
 import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -20,34 +26,72 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.kotlin.ui.theme.KotlinTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the screen orientation to portrait
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT.also { requestedOrientation = it }
         setContent {
             KotlinTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    NavigationBar()
+                    val authSuccessful = rememberSaveable { mutableStateOf(false) }
+                    if (authSuccessful.value) {
+                        NavigationBar()
+                    } else {
+                        Auth(
+                            //onAuthSuccess = { authSuccessful.value = true }
+                        )
+                    }
                 }
             }
         }
     }
 }
+
+@Composable
+fun Auth() {
+
+    var auth by remember { mutableStateOf(false) }
+
+    Column (
+        modifier = Modifier.background(Color.Red)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Auth Screen", fontSize = 22.sp)
+
+        Spacer(Modifier.height(8.dp))
+
+        Button(onClick = {
+
+        }) {
+            Text(text = "Login")
+        }
+    }
+}
+
+
 
 sealed class Screens (val screen: String) {
     data object HomeScreen: Screens("home")
@@ -69,6 +113,7 @@ val navItems = listOf(
     NavItem(R.drawable.ic_user_group, "Groups", Screens.GroupsScreen.screen)
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavigationBar() {
     val navigationController = rememberNavController()
@@ -124,14 +169,5 @@ fun NavigationBar() {
             composable(Screens.FriendsScreen.screen){FriendsScreen()}
             composable(Screens.GroupsScreen.screen){GroupsScreen()}
         }
-    }
-}
-
-
-@Preview(showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    KotlinTheme {
-        NavigationBar()
     }
 }
--- app/src/main/java/com/example/kotlin/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kotlin/ui/theme/Theme.kt
@@ -32,8 +32,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun KotlinTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
