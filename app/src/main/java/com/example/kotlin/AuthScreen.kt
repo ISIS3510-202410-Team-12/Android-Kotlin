de59150679b2e138ead01da64facface6b419bab
Laudarias <72948114+Laudarias@users.noreply.github.com>
2024-03-22 14:20:10 -0500

    9 implement biometric authentication (#10)
    Biometrics feature

---
+++ app/src/main/java/com/example/kotlin/AuthScreen.kt
@@ -0,0 +1,45 @@
+package com.example.kotlin
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/*
+@Composable
+fun Auth() {
+
+    var auth by remember { mutableStateOf(false) }
+
+    Column (
+        modifier = Modifier.background(Color.Red)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Auth Screen", fontSize = 22.sp)
+
+        Spacer(Modifier.height(8.dp))
+
+        Button(onClick = {
+
+        }) {
+            Text(text = "Login")
+        }
+    }
+}
+ */
