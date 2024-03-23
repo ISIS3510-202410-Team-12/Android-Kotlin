540307ae1f415398ed710e19a97b3feaa09d0a7d
Laudarias <l.ariasf@uniandes.edu.co>
2024-03-22 21:57:44 -0500

    Notification pops up prompted by button
    Notification services correctly accessed
    
    Co-Authored-By: jdcastellanosb73 <53950946+jdcastellanosb73@users.noreply.github.com>

---
+++ app/src/main/java/com/example/kotlin/NotificationService.kt
@@ -0,0 +1,34 @@
+package com.example.kotlin
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+const val NOTIFICATION_CHANNEL_ID = "ch-1"
+const val NOTIFICATION_CHANNEL_NAME = "Test Notification"
+const val NOTIFICATION_ID = 100
+const val REQUEST_CODE = 200
+
+class NotificationService (
+    private val context: Context
+) {
+    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    private val myIntent = Intent(context, MainActivity::class.java).apply { flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK}
+    private val pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE, myIntent, PendingIntent.FLAG_IMMUTABLE)
+
+    fun showNotification() {
+        val notification =
+            NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Test")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+}
