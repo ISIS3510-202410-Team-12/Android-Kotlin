798485b26a4c58b959d54d03f9649ca405f1b09f
Laudarias <l.ariasf@uniandes.edu.co>
2024-04-26 17:05:51 -0500

    Migration made succesfully

--- app/src/main/java/com/example/kotlin/MyApi.kt
+++ app/src/main/java/com/example/kotlin/MyApi.kt
@@ -1,14 +1,13 @@
 package com.example.kotlin
 
+import com.google.gson.JsonObject
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface MyApi {
-    @GET("http://34.133.121.100:3000/")
+    @POST("spaces")
     fun getFreeClassrooms(
-        @Query("dayOfWeek") dayOfWeek: String,
-        @Query("startTime") startTime: String,
-        @Query("endTime") endTime: String
+        @Body jsonBody: JsonObject
     ): Call<List<Classroom>>
 }
