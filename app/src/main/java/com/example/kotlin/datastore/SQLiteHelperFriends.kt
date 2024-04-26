241a60c705ff1aebd26cf765c6466e382374aa4f
jdcastellanosb73 <53950946+jdcastellanosb73@users.noreply.github.com>
2024-04-25 19:09:15 -0500

    fixed errors
    the error to save a friend in SQL lite was  fixed

--- app/src/main/java/com/example/kotlin/datastore/SQLiteHelperFriends.kt
+++ app/src/main/java/com/example/kotlin/datastore/SQLiteHelperFriends.kt
@@ -3,7 +3,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-class SQLiteHelperFriends(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class SQLiteHelperFriends(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
 
     companion object{
         private const val DATABASE_NAME ="Database.db"
@@ -14,7 +14,7 @@ class SQLiteHelperFriends(context: Context) : SQLiteOpenHelper(context, DATABASE
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        val createTableQuery = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMN_NAME TEXT, $COLUMN_NUMBER TEXT)"
+        val createTableQuery = "CREATE TABLE $TABLE_NAME ($ID INTEGER PRIMARY KEY AUTOINCREMENT, $NAME TEXT, $NUMBER TEXT)"
         db.execSQL(createTableQuery)
     }
 
@@ -23,11 +23,11 @@ class SQLiteHelperFriends(context: Context) : SQLiteOpenHelper(context, DATABASE
         onCreate(db)
     }
 
-    fun insertFriend(name: String, members: String) {
+    fun insertFriend(name: String, number: String) {
         val db = this.writableDatabase
         val values = ContentValues().apply {
-            put(COLUMN_NAME, name)
-            put(COLUMN_MEMBERS, members)
+            put(NAME, name)
+            put(NUMBER, number)
         }
         db.insert(TABLE_NAME, null, values)
         db.close()
